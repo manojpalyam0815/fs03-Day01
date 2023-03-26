1a3861181897d0b3d3a79b0a3503fda08db36436
manojpalyam0815 <palyammanojkumarmn@gmail.com>
2023-03-26 22:57:01 +0530

    the

--- Day 02/index.js
+++ Day 02/index.js
@@ -1,17 +1,17 @@
-// 1Create a JavaScript program that defines a function called "multiplyByTwo" that takes one parameter: "num". The function should return the input number multiplied by two.
+// Create a JavaScript program that defines a function called "multiplyByTwo" that takes one parameter: "num". The function should return the input number multiplied by two.
 
 function multiplyByTwo(num) {
     return num * 2;
     }
     
- // 2
+ // Create a JavaScript program that defines a function called "divideByTwo" that takes one parameter: "num". The function should return the input number divided by two.
     
     function divideByTwo(num) {
     return num / 2;
     }
     
     
-    // 3
+    // A function “findprime” is given with a parameter . Your function should print out all the prime from 1 till the number (parameter) present in the argument . 
     
     function findprime(num) {
     var primes = ""
